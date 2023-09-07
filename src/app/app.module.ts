90c0ec31841673519c9e69e6ba7b44e7b71a62ee
shayalieb <shayalieberman@gmail.com>
2023-09-07 11:21:41 -0400

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MovieInfoComponent } from './movie-info/movie-info.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    UserRegistrationFormComponent,
+    UserProfileComponent,
+    UserLoginFormComponent,
+    TopBarComponent,
+    NavigationComponent,
+    MovieInfoComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule,
